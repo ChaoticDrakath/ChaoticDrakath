00985d2394f8b3d0b453416ef4c6fe747be87563
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2018-07-25 08:26:25 +0300

    Update slugwrath.py

--- slugwrath.py
+++ slugwrath.py
@@ -127,7 +127,36 @@ async def clear(ctx, number):
     number = int(number) #Converting the amount of messages to delete to an integer
     async for x in client.logs_from(ctx.message.channel, limit = number+1):
         mgs.append(x)
-    await client.delete_messages(mgs)      
+    await client.delete_messages(mgs)
+    
+@client.command(pass_context=True)  
+@commands.has_permissions(send_messages=True)     
+
+async def serverinfo(ctx):
+    '''Displays Info About The Server!'''
+
+    server = ctx.message.server
+    roles = [x.name for x in server.role_hierarchy]
+    role_length = len(roles)
+
+    if role_length > 50: #Just in case there are too many roles...
+        roles = roles[:50]
+        roles.append('>>>> Displaying[50/%s] Roles'%len(roles))
+
+    roles = ', '.join(roles);
+    channelz = len(server.channels);
+    time = str(server.created_at); time = time.split(' '); time= time[0];
+
+    join = discord.Embed(description= '%s '%(str(server)),title = 'Server Name', colour = 0x6b009c);
+    join.set_thumbnail(url = server.icon_url);
+    join.add_field(name = '__Owner__', value = str(server.owner) + '\n' + server.owner.id);
+    join.add_field(name = '__ID__', value = str(server.id))
+    join.add_field(name = '__Member Count__', value = str(server.member_count));
+    join.add_field(name = '__Text/Voice Channels__', value = str(channelz));
+    join.add_field(name = '__Roles (%s)__'%str(role_length), value = roles);
+    join.set_footer(text ='Created: %s'%time);
+
+    return await client.say(embed = join);
  
        
 @client.command(pass_context = True)
