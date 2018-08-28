b1ad54ab61b58f1cacce69fffebdb3d728a55d78
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2018-08-28 05:32:47 +0000

    Update slugwrath.py

--- slugwrath.py
+++ slugwrath.py
@@ -27,7 +27,7 @@ async def status_task():
         await client.change_presence(game=discord.Game(name='in '+str(len(client.servers))+' servers'))
         await asyncio.sleep(5)
         await client.change_presence(game=discord.Game(name="Test", type=2))
-        
+        await asyncio.sleep(5)
         
 @client.event
 async def on_ready():
