61eb8be321df4a643d62b1d693ab599645683fa4
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2018-09-04 11:09:49 +0000

    Update slugwrath.py

--- slugwrath.py
+++ slugwrath.py
@@ -12,7 +12,7 @@ from discord import Game, Embed, Color, Status, ChannelType
 
 
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
-client = Bot(description="I am Dark Shroom!", command_prefix="DarkShroom  ", pm_help = True)
+client = Bot(description="I am Dark Shroom!", command_prefix="Shroom  ", pm_help = True)
 client.remove_command('help')
 
 players = {}
